9ab5709fe6bd59e79997720928461a9dfcd36375
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2023-09-08 23:54:03 +1000

    adding the admin registration from with header and footer as well

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LoginPage } from "./pages/login/LoginPage";
+import { AdminRegistration } from "./pages/admin-registration/AdminRegistration";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route to="/" element={<LoginPage />} />
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/register" element={<AdminRegistration />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/admin-registration/AdminRegistration.js
+++ src/pages/admin-registration/AdminRegistration.js
@@ -1,5 +1,14 @@
 import React from "react";
+import { Header } from "../../components/Header/Header";
+import { Footer } from "../../components/footer/Footer";
+import { Container } from "react-bootstrap";
 
 export const AdminRegistration = () => {
-  return <div>AdminRegistration</div>;
+  return (
+    <div>
+      <Header />
+      <Container />
+      <Footer />
+    </div>
+  );
 };
