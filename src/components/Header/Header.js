b5721a17c4f0c3f98507fa7647499704551666b8
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2023-09-12 00:08:47 +1000

    added the navbar on the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,20 @@
-import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Navbar collapseOnSelect expand="md" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="/">AdminCMS</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="/">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
