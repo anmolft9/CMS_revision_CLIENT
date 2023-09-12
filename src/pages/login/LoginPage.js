2bcb2ea7388167941ec7c6c995c5b9845be5b885
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2023-09-12 23:19:06 +1000

    added the header footer and the container on login page or root url

--- src/pages/login/LoginPage.js
+++ src/pages/login/LoginPage.js
@@ -1,5 +1,14 @@
 import React from "react";
+import { Header } from "../../components/Header/Header";
+import { Container } from "react-bootstrap";
+import { Footer } from "../../components/footer/Footer";
 
 export const LoginPage = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Header />
+      <Container className="page-main">Login Form coming soon</Container>
+      <Footer />
+    </div>
+  );
 };
