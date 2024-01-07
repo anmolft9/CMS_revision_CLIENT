13b969bcba4581ddc3f271a5ea68cd1001cc71b8
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2024-01-07 20:57:00 +0545

    working on the frontend> axios to send the data from admin registration form to the api/ backend

--- src/helpers/axiosHelper.js
+++ src/helpers/axiosHelper.js
@@ -1,1 +1,16 @@
 import axios from "axios";
+
+const apiProcessor = ({ method, url, data }) => {
+  try {
+    const response = axios({
+      method,
+      url,
+      data,
+    });
+  } catch (error) {
+    return {
+      status: "error",
+      message: error.message,
+    };
+  }
+};
