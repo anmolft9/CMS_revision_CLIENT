a0fdf5ba3234e0c61d611c37b909c7dc171bd1b5
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2024-01-14 19:56:39 +0545

    grab the data from the server and handle the frontedn part for the admin verification

--- src/pages/admin-registration/EmailVerification.js
+++ src/pages/admin-registration/EmailVerification.js
@@ -3,6 +3,13 @@ import { Header } from "../../components/Header/Header";
 import { Footer } from "../../components/footer/Footer";
 import { Container } from "react-bootstrap";
 
+///show the spinner
+///grab the c and e from the query string parameters
+//create and axios function to call the server
+
+//create api end point to receive the code
+//check if the combination of the email and the code exist on the user table, if yes then activate the user and send the verification email
+
 const EmailVerification = () => {
   return (
     <div>
