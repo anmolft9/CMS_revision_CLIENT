4f47640b515c74c02a96c97e95bfeb6c08f0c459
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2024-01-22 14:45:15 +0545

    adding the login form for the admin user

--- src/pages/login/LoginPage.js
+++ src/pages/login/LoginPage.js
@@ -1,13 +1,25 @@
 import React from "react";
 import { Header } from "../../components/Header/Header";
-import { Container } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 import { Footer } from "../../components/footer/Footer";
+import { Form } from "react-bootstrap";
+import { CustomInputField } from "../../components/customInputField/CustomInputField";
 
 export const LoginPage = () => {
   return (
     <div>
       <Header />
-      <Container className="page-main">Login Form coming soon</Container>
+      <Container className="page-main">
+        <div className="form">
+          <Form>
+            <CustomInputField />
+            <CustomInputField />
+            <Button variant="primary" type="submit">
+              Login
+            </Button>
+          </Form>
+        </div>
+      </Container>
       <Footer />
     </div>
   );
