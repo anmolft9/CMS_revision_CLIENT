7b22eb91106fdafef141f651cdbc27ade7fd604f
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2024-01-26 08:44:44 +0545

    added the reducers to the store

--- src/store.js
+++ src/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./pages/login/userSlice";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    admin: userReducer,
+  },
 });
 
 export default store;
