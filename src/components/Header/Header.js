665f636a6c6e7bf438fdbd7493e88b6fc3d48911
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2024-02-24 22:20:54 +0545

    created function to logout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,13 +2,20 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { setshowSideMenu } from "../../pages/systemState/SystemSlice";
+import { adminLogout } from "../../pages/login/userAction";
 
 export const Header = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handleShow = () => dispatch(setshowSideMenu(true));
   const { user } = useSelector((state) => state.admin);
+
+  const handleOnLogout = () => {
+    dispatch(adminLogout());
+    navigate("/");
+  };
 
   return (
     <Navbar collapseOnSelect expand="md" className="bg-body-tertiary">
@@ -27,7 +34,7 @@ export const Header = () => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             {user._id ? (
-              <Link className="nav-link" to="">
+              <Link className="nav-link" to="" onClick={handleOnLogout}>
                 Logout
               </Link>
             ) : (
