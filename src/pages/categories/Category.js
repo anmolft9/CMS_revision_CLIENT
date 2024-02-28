d504025cf1597e6606188c9228b74c09bdd1cea2
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2024-02-28 20:42:46 +0545

    adding the category component to display all the category datas

--- src/pages/categories/Category.js
+++ src/pages/categories/Category.js
@@ -1,6 +1,17 @@
 import React from "react";
 import { AdminLayout } from "../../components/layout/AdminLayout";
+import { CategoryTable } from "../../components/categoryTable/CategoryTable";
 
 export const Category = () => {
-  return <AdminLayout>Category</AdminLayout>;
+  return (
+    <AdminLayout>
+      <h2 className="py-3 d-flex justify-content-center">
+        Category Management
+      </h2>
+      {/* form */}
+      {/* table */}
+
+      <CategoryTable />
+    </AdminLayout>
+  );
 };
