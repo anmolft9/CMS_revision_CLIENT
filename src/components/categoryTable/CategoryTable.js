9ae9cda1ae7b098668859a8fc9015281018ffc05
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2024-02-29 18:49:37 +0545

    added all the items from the store to the table

--- src/components/categoryTable/CategoryTable.js
+++ src/components/categoryTable/CategoryTable.js
@@ -1,14 +1,44 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { Table, Row, Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
 import { getCategoriesAction } from "../../pages/categories/CategoryAction";
 
 export const CategoryTable = () => {
   const dispatch = useDispatch();
+
+  const { categories } = useSelector((state) => state.category);
+  // console.log(categories.length);
 
   useEffect(() => {
     // console.log("here");
     dispatch(getCategoriesAction());
   }, []);
 
-  return <div>CategoryTable</div>;
+  return (
+    <Row>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Status</th>
+            <th>Name</th>
+            <th>Level</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {categories.length > 0 &&
+            categories.map((item, i) => (
+              <tr>
+                <td>{item.status}</td>
+                <td>{item.name}</td>
+                <td>{item.parentId ? "childrean" : "parent"}</td>
+                <td>
+                  <Button variant="danger">Delete</Button>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </Table>
+    </Row>
+  );
 };
