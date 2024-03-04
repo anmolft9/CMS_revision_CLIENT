38a96b4d2d34d482cbf75e46cc42d779b8e28387
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2024-03-04 14:22:47 +0545

    added the parent category from the store to the form for manual selction

--- src/components/catForm/CatForm.js
+++ src/components/catForm/CatForm.js
@@ -1,27 +1,33 @@
 import React from "react";
 import { Form, Button, Col, Row } from "react-bootstrap";
-// import { Form } from "react-router-dom";
-import { CustomInputField } from "../customInputField/CustomInputField.js";
+import { useSelector } from "react-redux";
 
 export const CatForm = () => {
+  const { categories } = useSelector((state) => state.category);
+
   return (
-    <Form className="py-5 mb-5">
+    <Form className="py-5 mb-5 border p-3 shadow-lg rounded">
       <h5 className="mb-3">
         {" "}
         Add new Category
-        <hr />
+        <h r />
       </h5>
       <Row className="g-2">
         <Col md="2">
           <Form.Check name="status" label="status" type="switch" />
         </Col>
         <Col md="4">
-          <Form.Select name="parentId" label="parentId" type="dropdown">
-            <option>Parent category?</option>
+          <Form.Select name="parentId" label="parentId">
+            <option value="">Parent category?</option>
+
+            {categories.length > 0 &&
+              categories.map(
+                (item) => !item.parentId && <option>{item.name}</option>
+              )}
           </Form.Select>
         </Col>
         <Col md="4">
-          <Form.Control type="text" placeholder="Enter category" />
+          <Form.Control name="name" type="text" placeholder="Enter category" />
         </Col>
         <Col md="2">
           <Button variant="primary">Add</Button>
