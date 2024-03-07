bd6ac6c64de8791c49098385ff7917c31b7f24e6
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2024-03-07 13:24:12 +0545

    successfully added the form data to the db and displayed on the table

--- src/pages/categories/CategoryAction.js
+++ src/pages/categories/CategoryAction.js
@@ -3,8 +3,6 @@ import { setCategories } from "./CategorySlice.js";
 
 export const getCategoriesAction = () => async (dispatch) => {
   const { status, categories } = await fetchCategories();
-  // console.log(status, categories);
-  // console.log(typeof status);
 
   status === "success" && dispatch(setCategories(categories));
 };
